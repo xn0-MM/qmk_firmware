3cf07c3a84d2cd4661ed8351b3fded18c162cb13
xn0 <chanomm75@gmail.com>
2023-04-08 21:34:09 +0200

    Keymap v.0.6.10 Cleaning rules and enabling NKRO

--- keyboards/crkbd/keymaps/xn0/rules.mk
+++ keyboards/crkbd/keymaps/xn0/rules.mk
@@ -4,12 +4,13 @@ OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 WPM_ENABLE = yes
 EXTRAKEY_ENABLE = yes
-TAP_DANCE_ENABLE = no
 BOOTMAGIC_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 SWAP_HANDS_ENABLE = yes
+NKRO_ENABLE = yes
 
+TAP_DANCE_ENABLE = no
 RGBLIGHT_ENABLE = no   # Enable WS2812 RGB underlight.
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
@@ -19,15 +20,16 @@ MAGIC_ENABLE = no
 MUSIC_ENABLE = no 
 MIDI_ENABLE = no 
 BLUETOOTH_ENABLE = no
-NKRO_ENABLE = no
 BACKLIGHT_ENABLE = no
 AUTO_SHIFT_ENABLE = no
+UNICODE_ENABLE = no
+KEY_LOCK_ENABLE = no
+
 
 
 SRC += features/layer_lock.c
 SRC += features/oled.c
 SRC += features/hrmods.c
 SRC += features/functions.c
-#SRC += features/dances.c
 SRC += features/combos.c
 SRC += features/caps_word.c
